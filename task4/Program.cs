2a917beee67fc98517244e94e3218f77ac324fca
abylkaiymasel <47212476+abylkaiymasel@users.noreply.github.com>
2019-02-14 01:19:50 +0600

    Add files via upload

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           string file = "input.txt.rtf";
+            string path = @"/Users/ukadan.ali/Desktop/lab2/task4/task4/path/" + file ;
+            string path1 = @"/Users/ukadan.ali/Desktop/lab2/task4/task4/path1/" + file;
+
+         FileStream FS = new FileStream(path, FileMode.Create, FileAccess.Write);
+          FS.Close();
+            File.Copy(path, path1);
+            File.Delete(path);
+            }
+    }
+}
